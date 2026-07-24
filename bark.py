3861d24b00106a169f66946d5c38aab38cea0266
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-07-24 14:24:18 +0200

    feat(bark.py): parse args

--- bark.py
+++ bark.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python3
 
+import sys
+
+def error(s: str):
+    print(f"Error: {s} -- use 'bark.py -h' for more details")
+    sys.exit(1)
+
+def get_args():
+    args = sys.argv
+    if len(args) < 2:
+        error("no argument was provided")
+    for a in args[1:]:
+        match a:
+            case "-h" | "--help":
+                print("help")
+            case "record":
+                print("record")
+            case "compare":
+                print("compare")
+            case "history":
+                print("history")
+            case _:
+                error(f"unknown argument '{a}'")
+
 def main():
-    print("Hello world!")
+    get_args()
 
 if __name__ == "__main__":
     main()
